3f3260efad7f3bb2b0868190bfb029bd06e4e7b2
Crowdbotics <team@crowdbotics.com>
2020-03-27 18:17:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/uploadtool1_15133/MainActivity.java
@@ -0,0 +1,15 @@
+package com.uploadtool1_15133;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "uploadtool1_15133";
+    }
+}
